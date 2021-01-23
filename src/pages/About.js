aa657b6f50bbe2ee49d7fad534ca0b8c2552767e
Larraine Gonzales <larrainegonzales@hotmail.com>
2021-01-23 17:22:25 -0600

    updated head shot

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,7 +15,7 @@ const styles = {
     },
     pic: {
         width: "250px",
-        height: "400px",
+        height: "500px",
         display: "block",
         marginLeft: "auto",
         marginRight: "auto",
@@ -33,7 +33,7 @@ function About() {
             <div style={styles.jumbotron} className="jumbotron jumbotron-fluid rounded">
                 <div className="container">
                     <h1 style={styles.header} className="display-4 text-center">Larraine Gonzales</h1>
-                    <img style={styles.pic} src="https://user-images.githubusercontent.com/68169895/95691448-efe2cf80-0be4-11eb-8df6-bbb446341fb6.jpg" alt="Larraine!"></img>
+                    <img style={styles.pic} src="https://user-images.githubusercontent.com/68169895/105616810-252fb980-5d9f-11eb-84ea-4d1ee221952d.JPG" alt="Larraine!"></img>
                     <p style={styles.bio} className="lead">I am a certified Full Stack Developer. I thrive in the tech industry as I enjoy always learning the forever evolving tech tools! I have a background in marketing and sales, giving me a great eye for design for any desired market. </p>
                     <a style={styles.resume} href="https://drive.google.com/file/d/1Sc92BIYK3cvK3wkyVS3HdbtJjnb9mPEL/view?usp=sharing">View Resume</a>
                 </div>
