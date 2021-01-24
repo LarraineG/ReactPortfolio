9576598348e02dcb6492c44f2d917c943b551ba8
Larraine Gonzales <larrainegonzales@hotmail.com>
2021-01-24 09:46:33 -0600

    working on better mobile responsiveness

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,19 +11,22 @@ const styles = {
     },
     jumbotron: {
         backgroundImage: "linear-gradient(to bottom right, black, gray)",
-        boxShadow: "0px 0px 50px black"
+        boxShadow: "0px 0px 50px black",
+        backgroundColor: "black"
     },
     pic: {
-        width: "250px",
-        height: "530px",
+        width: "40%",
         display: "block",
         marginLeft: "auto",
         marginRight: "auto",
-        width: "40%"
+        mediaScreen: "max-width: 20%"
+
     },
     resume: {
-        marginLeft: "500px",
-        color: "black"
+        color: "black",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
     }
 }
 
@@ -33,9 +36,8 @@ function About() {
             <div style={styles.jumbotron} className="jumbotron jumbotron-fluid rounded">
                 <div className="container">
                     <h1 style={styles.header} className="display-4 text-center">Larraine Gonzales</h1>
-                    <img style={styles.pic} src="https://user-images.githubusercontent.com/68169895/105616810-252fb980-5d9f-11eb-84ea-4d1ee221952d.JPG" alt="Larraine!"></img>
-                    <p style={styles.bio} className="lead">I am a certified Full Stack Developer. I thrive in the tech industry as I enjoy always learning the forever evolving tech tools! I have a background in marketing and sales, giving me a great eye for design for any desired market. </p>
-                    <a style={styles.resume} href="https://drive.google.com/file/d/1Sc92BIYK3cvK3wkyVS3HdbtJjnb9mPEL/view?usp=sharing">View Resume</a>
+                    <img style={styles.pic} className="headshot" src="https://user-images.githubusercontent.com/68169895/105635464-894a9000-5e28-11eb-9b84-64ef1aa69c97.png" alt="Larraine!"></img>
+                    <p style={styles.bio} className="lead">I am a certified Full Stack Developer. I thrive in the tech industry as I enjoy always learning the forever evolving tech tools! I have a background in marketing and sales, giving me a great eye for design for any desired market. <a style={styles.resume} href="https://drive.google.com/file/d/1Sc92BIYK3cvK3wkyVS3HdbtJjnb9mPEL/view?usp=sharing">View Resume</a></p>
                 </div>
             </div>
         </Container>
